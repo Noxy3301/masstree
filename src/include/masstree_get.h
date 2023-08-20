8105bf7cc39eb2f2de04959c99e74ea8064949d6
Masahide Fukuyama <44726284+Noxy3301@users.noreply.github.com>
2023-08-21 07:15:53 +0900

    add: masstree_get
    関数だけ定義して実装途中だった

--- src/include/masstree_get.h
+++ src/include/masstree_get.h
@@ -17,5 +17,28 @@ FORWARD:
             goto RETRY;
         }
     }
-    
+    std::pair<SearchResult, LinkOrValue> result_lv = node->searchLinkOrValue(key);
+    SearchResult result = result_lv.first;
+    LinkOrValue lv = result_lv.second;
+    if ((node->getVersion() ^ version) > Version::has_locked) {
+        version = node->stableVersion();
+        BorderNode *next = node->getNext();
+        while (!version.deleted && next != nullptr && key.getCurrentSlice().slice >= next->lowestKey()) {
+            node = next;
+            version = node->stableVersion();
+            next = node->getNext();
+        }
+        goto FORWARD;
+    } else if (result == NOTFOUND) {
+        return nullptr;
+    } else if (result == VALUE) {
+        return lv.value;
+    } else if (result == LAYER) {
+        root = lv.next_layer;
+        key.next();
+        goto RETRY;
+    } else {
+        assert(result == UNSTABLE);
+        goto FORWARD;
+    }
 }
